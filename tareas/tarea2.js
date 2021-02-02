5f133b9b036be1d1d0d2084664d1eabf95e434d7
David <dloyolac@gmail.com>
2021-02-01 22:38:55 -0300

    to arrow functions

--- tareas/tarea2.js
+++ tareas/tarea2.js
@@ -1,25 +1,23 @@
 const misMascotas = {
     mascotas: ['perros','gatos','canarios','pez','serpiente'],
-    eliminarMascota: function(name){
+    eliminarMascota: function(name) {
         const index = this.mascotas.indexOf(name);
-        if (index > -1){
+        if (index > -1) {
             this.mascotas.splice(index, 1);
         }
         return this.mascotas;             
     },
-    eliminarUltimaMascota: function(){
+    eliminarUltimaMascota: function() {
         this.mascotas.pop();
         return this.mascotas;
     },
-    agregarMascota: function(nuevaMascota){
+    agregarMascota: function(nuevaMascota) {
         this.mascotas = [... this.mascotas, nuevaMascota];
         return this.mascotas;
     },
-    contadorMascotas: function(){
-        const regex = new RegExp("os$", 'g' )        
-        const matched = this.mascotas.filter(mascota => mascota.match(regex));
-        const result = `En el arreglo hay ${matched.length} mascotas que terminan con 'os'`;
-        return result;
+    contadorMascotas: function() {
+        const matched = this.mascotas.filter(mascota => mascota.match(new RegExp('os$', 'g')));
+        return `En el arreglo hay ${matched.length} mascotas que terminan con 'os'`;
     },
 }
 
@@ -49,18 +47,13 @@ const informacionPersona = function({
     }, 
     cargo, 
     gustos, 
-    hijos,}){
-    
-    const revisarGustos = function(){
-        return gustos ? `le gusta (${gustos[0]}) y ${gustos.length - 1} más` : '(no tiene gustos)'
-    };
-    const revisarHijos = function(){
-        return hijos ? `tiene ${hijos.length} hijo(s)` : 'no tiene hijos';
-    };
+    hijos,}) {    
+    const revisarGustos = () => gustos ? `le gusta (${gustos[0]}) y ${gustos.length - 1} más` : '(no tiene gustos)';
+    const revisarHijos = () => hijos ? `tiene ${hijos.length} hijo(s)` : 'no tiene hijos';    
     return `El trabajador (${nombre}) trabaja en (${empresaNombre}) con cargo (${cargo}) y ${revisarGustos()}, (${revisarHijos()})`;
 }
 
-const informacionTrabajador = function({
+const informacionTrabajador = ({
     nombre, 
     empresa: {
         ubicación: {
@@ -71,14 +64,10 @@ const informacionTrabajador = function({
     }, 
     cargo, 
     gustos, 
-    hijos,}){
-    const revisarClientes = function(){
-        const thisClientes = clientes.map(function(cliente){
-            return `(${cliente})`;
-        })
+    hijos,}) => {    
+    const revisarClientes = () => {
+        const thisClientes = clientes.map(cliente => `(${cliente})`)
         return thisClientes.slice(0, thisClientes.length - 1).join(", ") + " y " + thisClientes[thisClientes.length - 1];
-    };        
-  
+    }
     return `El trabajador (${nombre}) va a su trabajo en (${comuna}), es (${cargo}), en el puesto (${puesto}), trabaja con ${revisarClientes()}`;
 }
-
